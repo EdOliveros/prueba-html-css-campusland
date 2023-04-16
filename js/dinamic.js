5ebc4346081277e16f70aa7aa0245ff7691a2cba
ed oliveros <edgarolimoy98@gmail.com>
2023-04-16 11:26:32 -0500

    feat: :art: Javascript implementado para renderizar modales con contenido dinamico

--- js/dinamic.js
+++ js/dinamic.js
@@ -21,8 +21,62 @@ for(let i = 0; i < servicios.length; i++) {
     const boton = document.createElement('button')
     boton.textContent = 'Ver datos'
     boton.addEventListener('click', () => {
-        console.log('Boton pulsado')
 
+
+        const modalContainer = document.createElement('div');
+        modalContainer.id = 'modal-container';        
+    
+        const modalContenido = document.createElement('div');
+        modalContenido.className = 'modal';
+    
+        modalContainer.appendChild(modalContenido)
+    
+        
+        const contenido = document.createElement('div')
+        contenido.id = 'modal-contenido'
+        modalContenido.appendChild(contenido)
+    
+        const tabla = document.createElement('table');
+    
+        const encabezado = document.createElement('tr');
+    
+        const thNombre = document.createElement('th');
+        thNombre.textContent = 'Nombre';
+        encabezado.appendChild(thNombre);
+        const thDetalles = document.createElement('th');
+        thDetalles.textContent = 'Detalles';
+        encabezado.appendChild(thDetalles);
+        const thDisponibilidad = document.createElement('th');
+        thDisponibilidad.textContent = 'Disponibilidad';
+        encabezado.appendChild(thDisponibilidad);
+        tabla.appendChild(encabezado);
+    
+        const contenidotr = document.createElement('tr');
+        const tdNombre = document.createElement('td');
+        tdNombre.textContent = servicios[i].name;
+        contenidotr.appendChild(tdNombre);
+        const tdDetalles = document.createElement('td');
+        tdDetalles.textContent = servicios[i].descripcion;
+        contenidotr.appendChild(tdDetalles);
+        const tdDisponibilidad = document.createElement('td');
+        tdDisponibilidad.textContent = servicios[i].disponibilidad;
+        contenidotr.appendChild(tdDisponibilidad);
+        tabla.appendChild(contenidotr);
+    
+        const modalesContenedor = document.getElementById('contenedor-modales')
+
+        const botonCerrar = document.createElement('button');
+        botonCerrar.textContent = 'Cerrar';
+        botonCerrar.classList.add('close')
+        botonCerrar.addEventListener('click', () => {
+            console.log('Cerrar ahora')
+            modalesContenedor.innerHTML = ' '
+        });
+    
+        contenido.appendChild(tabla);
+        contenido.appendChild(botonCerrar);
+    
+        modalesContenedor.appendChild(modalContainer)
     })
     tarjeta.appendChild(boton)
 
@@ -30,109 +84,10 @@ for(let i = 0; i < servicios.length; i++) {
 
 
 
-    var id = `${servicios[i].id}`
-
-
-    const modalContainer = document.createElement('div');
-    modalContainer.id = 'modal-container';
-    modalContainer.classList.add('d-none', `${id}`)
-
-    const modalContenido = document.createElement('div');
-    modalContenido.className = 'modal';
-
-    modalContainer.appendChild(modalContenido)
-
     
-    const contenido = document.createElement('div')
-    contenido.id = 'modal-contenido'
-    modalContenido.appendChild(contenido)
-
-    const tabla = document.createElement('table');
-
-    const encabezado = document.createElement('tr');
-
-    const thNombre = document.createElement('th');
-    thNombre.textContent = 'Nombre';
-    encabezado.appendChild(thNombre);
-    const thDetalles = document.createElement('th');
-    thDetalles.textContent = 'Detalles';
-    encabezado.appendChild(thDetalles);
-    const thDisponibilidad = document.createElement('th');
-    thDisponibilidad.textContent = 'Disponibilidad';
-    encabezado.appendChild(thDisponibilidad);
-    tabla.appendChild(encabezado);
-
-    const contenidotr = document.createElement('tr');
-    const tdNombre = document.createElement('td');
-    tdNombre.textContent = servicios[i].name;
-    contenidotr.appendChild(tdNombre);
-    const tdDetalles = document.createElement('td');
-    tdDetalles.textContent = servicios[i].descripcion;
-    contenidotr.appendChild(tdDetalles);
-    const tdDisponibilidad = document.createElement('td');
-    tdDisponibilidad.textContent = servicios[i].disponibilidad;
-    contenidotr.appendChild(tdDisponibilidad);
-    tabla.appendChild(contenidotr);
 
 
-    const botonCerrar = document.createElement('button');
-    botonCerrar.textContent = 'Cerrar';
-    botonCerrar.addEventListener('click', () => {
-        console.log('Boton pulsado')
-    });
-
-    contenido.appendChild(tabla);
-    contenido.appendChild(botonCerrar);
-
-    tarjetasContainer.appendChild(modalContainer)
-
-
-
-
-    
-//     modal.innerHTML += `
-//     <div id="modal-container" class="d-none ">
-        
-//     <div class="modal">
-//         <div id="modal-contenido">
-//             <table>
-//                 <tr>
-//                     <th>
-//                         Nombre
-//                     </th>
-//                     <th>
-//                         Detalles
-//                     </th>
-//                     <th>
-//                         Disponibilidad
-//                     </th>
-//                 </tr>
-//                 <tr>
-//                     <td>
-//                         ${servicios[i].name}
-//                     </td>
-//                     <td>
-//                         ${servicios[i].descripcion}
-//                     </td>
-//                     <td>
-//                         ${servicios[i].disponibilidad}
-//                     </td>
-//                 </tr>
-//             </table>
-//             <button onclick='render'>Cerrar</button>
-//         </div>
-//     </div>
-// </div> 
-
-//     `
 }
-
-
-
-
-
-
-
 
 // script accordion
 
